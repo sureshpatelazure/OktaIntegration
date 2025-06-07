39af75e5879126aec3deb460026663ca923dabb7
sureshpatelgcp <sureshpatelazure@gmail.com>
2025-06-07 17:29:31 +0530

    Added OIDC

--- OktaOpenIDConnectDemo/OktaOpenIDConnectDemo/Controllers/HomeController.cs
+++ OktaOpenIDConnectDemo/OktaOpenIDConnectDemo/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OktaOpenIDConnectDemo.Models;
 
 namespace OktaOpenIDConnectDemo.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
--- OktaOpenIDConnectDemo/OktaOpenIDConnectDemo/Program.cs
+++ OktaOpenIDConnectDemo/OktaOpenIDConnectDemo/Program.cs
@@ -1,7 +1,29 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = "openid";
+}).AddCookie()
+.AddOpenIdConnect("openid", options =>
+{
+    var oktaDomain = builder.Configuration["Okta:Domain"];
+    options.Authority = $"{oktaDomain}/oauth2/default"; // Replace with your Okta domain
+    options.ClientId = builder.Configuration["Okta:ClientId"].ToString();   // Replace with your Client ID
+    options.ClientSecret = builder.Configuration["Okta:ClientSecret"].ToString();  // Replace with your Client Secret
+    options.ResponseType = OpenIdConnectResponseType.Code;
+    options.SaveTokens = true;
+    options.Scope.Add("openid");
+    options.Scope.Add("profile");
+    options.Scope.Add("email");
+});
 
 var app = builder.Build();
 
@@ -16,8 +38,10 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
